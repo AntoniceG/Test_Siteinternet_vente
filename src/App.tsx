beab7825e3f911ab4bd7ce5c270f472459c796d6
AntoniceG <geoantonicelli@gmail.com>
2026-02-05 18:56:00 +0100

    Update build with HashRouter for GH Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import { Emerald } from "./screens/Emerald/Emerald";
 import { Home } from "./screens/Home/Home";
 import { OptiPro } from "./screens/OptiPro/OptiPro";
 
 export const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/emerald" element={<Emerald />} />
         <Route path="/optipro" element={<OptiPro />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
